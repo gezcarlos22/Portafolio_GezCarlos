fb67d7ed2ee9bb08f7f6e3bf1ffdc38565afcc04
gezcarlos22 <ceg290698@gmail.com>
2023-04-30 22:55:05 -0300

    cambio con boot
    cambio boot

--- Proyecto_Gez/Front End/src/app/app.module.ts
+++ Proyecto_Gez/Front End/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LogoAPComponent,
+
   ],
   imports: [
     BrowserModule,
