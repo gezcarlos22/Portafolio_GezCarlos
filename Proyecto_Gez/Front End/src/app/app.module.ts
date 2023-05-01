744e2776c64b5f83923c7361b91b959d7b9c7818
gezcarlos22 <ceg290698@gmail.com>
2023-05-01 03:59:49 -0300

    cambios
    probando cambios

--- Proyecto_Gez/Front End/src/app/app.module.ts
+++ Proyecto_Gez/Front End/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    HeaderComponent,
+    PerfilComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    PortafolioComponent,
   ],
   imports: [
     BrowserModule,
